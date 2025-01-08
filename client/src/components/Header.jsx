08ddbdce31756bcd1a0ac9cf47ea334c15847581
Sukumar Biswas <sukumarbsws98@gmail.com>
2025-01-08 20:18:00 +0600

    frontend 2.04

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import logo from '../assets/logo.png'
 import Search from './Search'
-import { Link } from 'react-router-dom'
+import { FaRegCircleUser } from "react-icons/fa6";
+import { IoCartSharp } from "react-icons/io5";
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import useMobile from '../hooks/useMobile';
 
 
 const Header = () => {
+
+    const [isMobile] = useMobile();
+    const location = useLocation()
+    const isSearchPage = location.pathname === "/search"
+    const navigate = useNavigate()
+
+    const redirectToLoginPage = ()=>{
+        navigate("/login")
+    }
+
   return (
-    <header className='h-20 shadow-md sticky top-0'>
-        <div className='container mx-auto flex items-center h-full px-2 justify-between'>
+    <header className='h-24 lg:h-20 lg:shadow-md sticky top-0 flex flex-col justify-center gap-1 bg-white'>
+        {
+            !(isSearchPage && isMobile) &&(
+                <div className='container mx-auto flex items-center px-2 justify-between'>
             {/* logo */}
         <div className='h-full'>
             <Link to={"/"} className='h-full flex justify-center items-center'>
@@ -16,14 +31,37 @@ const Header = () => {
             </Link>
         </div>
         {/* search */}
-        <div>
+        <div className='hidden lg:block'>
             <Search />
         </div>
 
         {/* login and mycart */}
         <div>
-            Login and my cart
+            {/* user icon  will be displayed in only mobile  */}
+            <button className='text-neutral-600 lg:hidden'>
+                <FaRegCircleUser size={25}/>
+            </button>
+
+            {/* user icon  will be displayed in only desktop */}
+            <div className='hidden lg:flex items-center gap-10'>
+                <button onClick={redirectToLoginPage} className='text-lg px-2'>Login</button>
+                <button className='flex items-center gap-2 bg-green-800 hover:bg-green-700 px-4 py-3 rounded text-white'>
+                    <div className='animate-bounce'>
+                        {/* add to cart icon */}
+                        <IoCartSharp size={26}/>
+                    </div>
+                    <div className='font-semibold'>
+                        <p>My cart</p>
+                    </div>
+                </button>
+            </div>
         </div>
+        </div>
+            )
+        }
+        
+        <div className='container mx-auto px-2 lg:hidden'>
+            <Search/>
         </div>
     </header>
   )
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <section className=''>
+
+    </section>
+  )
+}
+
+export default Register

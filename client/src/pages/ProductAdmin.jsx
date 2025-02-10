5c46dbd35bf16747e5c7bf1a493c2f3b82d21f5e
skbiswas0509 <sukumarbsws98@gmail.com>
2025-02-10 19:50:40 +0600

    3

--- client/src/pages/ProductAdmin.jsx
+++ client/src/pages/ProductAdmin.jsx
@@ -3,10 +3,14 @@ import AxiosToastError from '../utils/AxiosToastError'
 import Axios from '../utils/Axios'
 import Loading from '../components/Loading'
 import ProductCardAdmin from '../components/ProductCardAdmin'
+import { IoSearch } from "react-icons/io5";
+
 
 const [productData, setProductData] = useState([])
 const [page, setPage] = useState(1)
 const [loading, setLoading] = useState(false)
+const [totalPageCount, setTotalPageCount] = useState(1)
+const [search, setSearch] = useState("")
 
 const fetchProductData = async () => {
     try {
@@ -15,7 +19,8 @@ const fetchProductData = async () => {
             ...SummaryApi.getProduct,
             data: {
                 page: page,
-                limit : 12
+                limit : 12,
+                search : search 
             }
         })
         const { data: responseData } = response
@@ -32,12 +37,50 @@ const fetchProductData = async () => {
 
 useEffect(() => {
     fetchProductData()
-}, [])
+}, [page])
+
+const handleNext =()=>{
+    if(page !== totalPageCount){
+    setPage(preve => preve + 1)
+    }
+}
+const handlePrevious =()=>{
+    if(page > 1){
+    setPage(preve => preve - 1)
+    }
+}
+
+const handleOnChange = ()=>{
+    const {value} = e.target
+    setSearch(value)
+    setPage(1)
+}
+
+useEffect(()=>{
+    let flag = true
+
+    const interval = setTimeout(() => {
+        if(flag){
+            fetchProductData()
+            flag = false
+        }
+    }, 300);
+    
+    return ()=>{
+        clearTimeout(interval)
+    }
+},[search])
+
 const ProductAdmin = () => {
     return (
         <section>
-            <div className='p-2 bg-white shadow-md items-center justify-between'>
+            <div className='p-2 bg-white shadow-md flex items-center justify-between gap-4'>
                 <h2 className='font-semibold'>Products</h2>
+                <div className='h-full min-w-24 max-w-56 w-full ml-auto bg-blue-50 px-4 flex items-center gap-3 py-2 rounded border focus-within:bg-primary-200'>
+                    <IoSearch size={25}/>
+                    <input type="text" placeholder='Search product here...'
+                    className='h-full w-full outline-none bg-transparent'/>
+                </div>
             </div>
             {
                 loading && (
@@ -45,18 +88,26 @@ const ProductAdmin = () => {
                 )
             }
             <div className='p-4 bg-blue-50'>
-                <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4'>
-                    {
-                        productData.map((product, index) => {
-                            return (
-                                <ProductCardAdmin data={product} />
-                            )
-                        })
-                    }
+                <div className='min-h-[55vh]'>
+                    <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4'>
+                        {
+                            productData.map((product, index) => {
+                                return (
+                                    <ProductCardAdmin data={product} />
+                                )
+                            })
+                        }
+                    </div>
                 </div>
+                
                 <div className='flex justify-between my-4'>
-                    <button className='border border-primary-200 px-4 py-1 hover:bg-primary-200'>Previous</button>
-                    <button className='border border-primary-200 px-4 py-1 hover:bg-primary-200'>Next</button>
+                    <button onClick={handlePrevious} className='border border-primary-200 px-4 py-1 hover:bg-primary-200'>Previous</button>
+                    <button className='w-full bg-slate-100'>{page}/{totalPageCount}</button>
+                    <button onClick={handleNext} 
+                    className='border border-primary-200 px-4 py-1 hover:bg-primary-200'
+                    value={search}
+                    onChange={handleOnChange}
+                    >Next</button>
                 </div>
             </div>
 
